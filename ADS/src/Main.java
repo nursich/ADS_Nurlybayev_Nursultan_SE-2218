ab44fc74abda657570fb6617c1cc71e7840107c8
nursich <nurlibaiem04@mail.ru>
2023-04-09 20:56:28 +0600

    Task 5

--- ADS/src/Main.java
+++ ADS/src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int n = 5;   // initialize a number
-        System.out.println(findFactorial(n));
+        int n2= 17;
+        System.out.println("The " + n + "-th element in Fibonacci sequence is: " + fibonacciEl(n));  // print a result
+        System.out.println("The " + n2 + "-th element in Fibonacci sequence is: " + fibonacciEl(n2));
     }
-    public static int findFactorial(int n) {
-        if (n == 1){  // base condition
+    public static int fibonacciEl(int n) {
+        if (n == 0) {      // base condition n1
+            return 0;
+        }
+        else if (n == 1) {      // base condition n2
             return 1;
         }
-        else {
-            return n * findFactorial(n - 1); // using recursion in order to multiply 'n' to 'n-1' and etc
+        else {     // recursion in order to find n-th element in Fibo sequence
+            return fibonacciEl(n - 1) + fibonacciEl(n - 2);
         }
     }
 }
