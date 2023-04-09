da980ca604dbb97e71e0f77d9d586b1ee4570fa3
nursich <nurlibaiem04@mail.ru>
2023-04-09 19:46:51 +0600

    Task 1

--- ADS/src/Main.java
+++ ADS/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        // hi
+        int[] arr = {10, 1, 32, 3, 45}; // given array
+        System.out.println("Minimum in the array is:" + findMin(arr, arr.length)); // print solution on the screen
+    }
+    public static int findMin(int[] arr, int n) {
+        if (n == 1) {       // base condition in order not to break a code
+            return arr[0];
+        }
+        else {
+            int minValue = findMin(arr, n - 1);   // variable calls the func
+            return Math.min(minValue, arr[n - 1]);   // сomparing one element with 'n-1' el
+        }
     }
 }
