d205d371bcaae1c7bd978b6639cafef90dd5b9d8
nursich <nurlibaiem04@mail.ru>
2023-04-09 20:24:32 +0600

    Task 3

--- ADS/src/Main.java
+++ ADS/src/Main.java
@@ -1,14 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {3, 2, 4, 1}; // given array
-        System.out.println(arrAverage(arr, arr.length)); // print result
-
-    }
-    public static float arrAverage(int[] arr, int n) {
-        float sum = 0;  // variable to count sum numbers in array
-        for(int i = 0; i < n; i++) {
-            sum += arr[i];  // iterating every element in array and incrementing it to the variable
+        int n = 7;   // initialize a number
+        int n2 = 10;   // initialize a number
+        if (isPrime(n)) {   // Check the number
+            System.out.println("The number: " + n + " is prime");
+        } else {
+            System.out.println("The number:" + n + " is not prime");
         }
-        return (sum / arr.length);  // returning the solution
+        if (isPrime(n2)) {   // Check the number
+            System.out.println("The number: " + n2 + " is prime");
+        } else {
+            System.out.println("The number: " + n2 + " is not prime");
+        }
+    }
+    public static boolean isPrime(int n) {
+        boolean st = true;  // boolean variable in order to check
+        for(int i = 2; i < n; i++) {
+            if (n % i == 0) {   // condition: if any number that divides to 'n' met = not prime
+                st = false;
+                break;
+            }
+        }
+        return st;
     }
 }
