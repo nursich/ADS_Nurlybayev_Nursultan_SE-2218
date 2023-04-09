7e6bd2acd5bfe659b4e892d88439dc720879e2bd
nursich <nurlibaiem04@mail.ru>
2023-04-09 22:42:03 +0600

    Task 9

--- ADS/src/Main.java
+++ ADS/src/Main.java
@@ -1,23 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        String s = "123456";
-        int n = s.length() - 1;
-        if(ifStringOnlyDigits(s, n)) {
-            System.out.println("Yes");
-        }
-        else {
-            System.out.println("No");
-        }
+        int n = 7;
+        int k = 3;
+        System.out.println("The result of C(" + n + "," + k + ") is " + binomialCoeff(n, k));
     }
-    public static boolean ifStringOnlyDigits(String s, int n) {
-        if(n == 1) {
-            return Character.isDigit(s.charAt(0));
-        }
-        if(Character.isDigit(s.charAt(n))) {
-            return ifStringOnlyDigits(s, n - 1);
+    public static int binomialCoeff(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
         }
         else {
-            return false;
+            return binomialCoeff(n - 1, k - 1) + binomialCoeff(n - 1, k);
         }
     }
 }
